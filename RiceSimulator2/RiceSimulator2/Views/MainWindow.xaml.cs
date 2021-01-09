227a48e6ac1312141fade5d588346de74a61052b
SolaireSauce <samuel.porchet11@gmail.com>
2021-01-09 22:48:48 +0100

    Yes

--- RiceSimulator2/RiceSimulator2/Views/MainWindow.xaml.cs
+++ RiceSimulator2/RiceSimulator2/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RiceSimulator2
+namespace RiceSimulator2.Views
 {
     public partial class MainWindow : Window
     {
